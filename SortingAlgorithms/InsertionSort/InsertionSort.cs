2b7eaa1c0b480fa5dd1210e487dee0df0313f05f
Lukasz Dyga <lukasz.dyga@chorzow.merito.pl>
2025-05-16 21:57:26 +0200

    updates

---
+++ SortingAlgorithms/InsertionSort/InsertionSort.cs
@@ -0,0 +1,58 @@
+﻿namespace SortingAlgorithms
+{
+    public class InsertionSort
+    {
+        public void Sort(int[] array)
+        {
+            for (int j = 1; j < array.Length; j++)
+            {
+                int key = array[j];
+                int i = j - 1;
+
+                while (i >= 0 && array[i] > key)
+                {
+                    array[i + 1] = array[i];
+                    i--;
+                }
+
+                array[i + 1] = key;
+
+                string left = string.Join(" ", array.Take(j));
+                string right = string.Join(" ", array.Skip(j));
+                Console.WriteLine($"Step {j}: {left} # {right}");
+            }
+        }
+
+        public void SortWithSwap(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && array[j - 1] > array[j])
+                {
+                    Swap(array, j, j - 1);
+                    j--;
+                }
+
+                string left = string.Join(" ", array.Take(i));
+                string right = string.Join(" ", array.Skip(i));
+                Console.WriteLine($"Step {i}: {left} # {right}");
+            }
+        }
+
+        private void Swap(int[] array, int a, int b)
+        {
+            int temp = array[a];
+            array[a] = array[b];
+            array[b] = temp;
+        }
+    }
+
+ /*
+1. In the first step the sorted array consists on one element [0] from array[0] and the key is the second element [1].
+1. In the classical approach sorted array is on the left side of the key.
+2. The sorted array changed its length and in each step has the range [0..j-1] depending on the index of actual element [j].
+3. From usorted array key is the first element of the unsorted array in the conqrete interation j, key=array[j]. 
+*/
+
+}
---
+++ SortingAlgorithms/InsertionSort/Program.cs
@@ -0,0 +1,53 @@
+﻿using SortingAlgorithms;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        // Generate random numbers for the unsorted array
+        Random random = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(-10, 10);
+        }
+
+        Console.WriteLine("Array before sorting:");
+        Console.WriteLine(string.Join(" ", numbers) + "\n");
+
+        // Create two independent copies the same array
+        int[] arrayShift = new int[numbers.Length];
+        int[] arraySwap = new int[numbers.Length];
+
+        Array.Copy(numbers, arrayShift, numbers.Length);
+        Array.Copy(numbers, arraySwap, numbers.Length);
+
+        InsertionSort sorter = new InsertionSort();
+
+        // the sorting method with shift
+        Stopwatch timeShift = Stopwatch.StartNew();
+        Console.WriteLine("Standard approach with shift\n");
+        sorter.Sort(arrayShift);
+        timeShift.Stop();
+        Console.WriteLine("\nArray after insertion sort:" + string.Join(" ", arrayShift));
+
+        Console.WriteLine("\n\nArray before sorting:");
+        Console.WriteLine(string.Join(" ", numbers) + "\n");
+
+        // calling the sorting method with swap
+        Stopwatch timeSwap = Stopwatch.StartNew();
+        Console.WriteLine("Swap approach\n");
+        sorter.SortWithSwap(arraySwap);
+        timeSwap.Stop();
+
+        Console.WriteLine("\nArray after insertion sort:" + string.Join(" ", arraySwap));
+
+        Console.WriteLine("\nExecution time:");
+        Console.WriteLine($"Insert sort with shift:{timeShift.Elapsed.TotalMilliseconds} ms");
+        Console.WriteLine($"Insert sort with swap: {timeSwap.Elapsed.TotalMilliseconds} ms");
+
+        Console.ReadLine();
+    }
+}

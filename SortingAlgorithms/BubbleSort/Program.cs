895476fb222cbcb86b8e7a4e8edbb91d4e074cb2
Lukasz Dyga <lukasz.dyga@chorzow.merito.pl>
2025-08-27 16:20:43 +0200

    bubble sort

--- SortingAlgorithms/BubbleSort/Program.cs
+++ SortingAlgorithms/BubbleSort/Program.cs
@@ -1,3 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿int[] original = { 5, 3, 8, 4, 2, 4, 6 };
+
+Console.WriteLine("Original Table: " + string.Join(", ", original));
+
+int[] sorted = (int[])original.Clone();
+
+BubbleSort(sorted);
+
+Console.WriteLine("Sorted array: " + string.Join(", ", sorted));
+void BubbleSort(int[] input)
+{
+    for (int i = 0; i < input.Length - 1; i++)
+    {
+        for (int j = 0; j < input.Length - 1 - i; j++)
+        {
+            if (input[j] > input[j + 1])
+            {
+                int temp = input[j];
+                input[j] = input[j + 1];
+                input[j + 1] = temp;
+            }
+        }
+    }
+}
